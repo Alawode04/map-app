91396831c305ea8aad437411f117c49e6fe822ad
Alawode04 <alawodepuritymoyosola@gmail.com>
2023-11-21 13:41:08 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MapComponent from './components/MapComponent'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <MapComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+const MapComponent = () => {
+
+
+  return (
+   
+    <div>
+      <p>
+        <iframe
+          id="map-canvas"
+          className="map_part"
+          width="600"
+          height="450"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=Surulere, Lagos, Nigeria&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          Powered by{" "}
+          <a href="https://www.googlemapsgenerator.com">
+            html embed google maps
+          </a>{" "}
+          and{" "}
+          <a href="https://xn--samla-ln-utan-uc-job.se/">samla lån utan uc</a>
+        </iframe>
+      </p>
+    </div>
+  );
+};
+
+export default MapComponent;
